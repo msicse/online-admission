21dc32f3930bb874dbd7fd960361870e29480ee3
Md. Sumon Islam <eubsumongmail.com>
2019-02-20 08:01:24 +0600

    updated-20-02-19

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -37,6 +37,26 @@
                                 <div class="tab-content" id="myTabContent">
                                   <div class="tab-pane fade show active" id="program-details" role="tabpanel" aria-labelledby="home-tab">
                                       <div class="form-group row ">
+                                          <label for="program" class="col-md-4 col-form-label text-md-right">{{ __('Program') }}</label>
+
+                                          <div class="col-md-8">
+                                              <select name="program" id="program" class="custom-select custom-select-sm" size="5">
+                                                    <option value="0" selected>Select One</option>
+                                                    <option value="1">B.Sc. in Textile Engg. (Regular)</option>
+                                                    <option value="11">B.Sc. in Textile Engg. (Diploma Holders)</option>
+                                                    <option value="2">B.Sc. in EEE (Regular)</option>
+                                                    <option value="21">B.Sc. in EEE (Diploma Holders)</option>
+                                                    <option value="3">B.Sc. in Civil Engg. (Regular)</option>
+                                                    <option value="31">B.Sc. in Civil Engg. (Diploma Holders)</option>
+                                                    <option value="4">B.Sc. in CSE (Regular)</option>
+                                                    <option value="41">B.Sc. in CSE (Diploma Holders)</option>
+                                                    <option value="5">B.Sc. in MIPE (Regular)</option>
+                                                    <option value="51">B.Sc. in MIPE (Diploma Holders)</option>
+                                              </select>
+                                              <span id="error-program" class="" role="alert"></span>
+                                          </div>
+                                      </div>
+                                      <div class="form-group row ">
                                           <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Semester') }}</label>
 
                                           <div class="col-md-8">
@@ -65,26 +85,7 @@
                                               <span id="error-year" class="invalid-feedback" role="alert"></span>
                                           </div>
                                       </div>
-                                      <div class="form-group row ">
-                                          <label for="program" class="col-md-4 col-form-label text-md-right">{{ __('Program') }}</label>
 
-                                          <div class="col-md-8">
-                                              <select name="program" id="program" class="custom-select custom-select-sm" size="5">
-                                                    <option value="0" selected>Select One</option>
-                                                    <option value="1">B.Sc. in Textile Engg. (Regular)</option>
-                                                    <option value="11">B.Sc. in Textile Engg. (Diploma Holders)</option>
-                                                    <option value="2">B.Sc. in EEE (Regular)</option>
-                                                    <option value="21">B.Sc. in EEE (Diploma Holders)</option>
-                                                    <option value="3">B.Sc. in Civil Engg. (Regular)</option>
-                                                    <option value="31">B.Sc. in Civil Engg. (Diploma Holders)</option>
-                                                    <option value="4">B.Sc. in CSE (Regular)</option>
-                                                    <option value="41">B.Sc. in CSE (Diploma Holders)</option>
-                                                    <option value="5">B.Sc. in MIPE (Regular)</option>
-                                                    <option value="51">B.Sc. in MIPE (Diploma Holders)</option>
-                                              </select>
-                                              <span id="error-program" class="" role="alert"></span>
-                                          </div>
-                                      </div>
                                       <br>
                                       <div class="col text-center">
                                           <button type="button" name="btn-program-details" id="btn-program-details"  class="btn btn-info ">Next</button>
