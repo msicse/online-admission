ed08830fea530371289390bbfdd541c0f04431d8
Sumon <eubsumon@gmail.com>
2019-03-25 17:16:27 +0600

    updated

--- resources/views/frontend/admission/from/cv.blade.php
+++ resources/views/frontend/admission/from/cv.blade.php
@@ -76,6 +76,47 @@
 
                             </div>
                             <div class="card-header">Academic Information</div>
+                            <div class="row">
+                                <div class="col">
+                                    <table class="table table-striped table-bordered">
+                                      <thead>
+                                        <tr>
+                                          <th scope="col">#</th>
+                                          <th scope="col">Title</th>
+                                          <th scope="col">Group / Concentration</th>
+                                          <th scope="col">Result</th>
+                                          <th scope="col">Passing Year</th>
+                                        </tr>
+                                      </thead>
+                                      <tbody>
+                                        <tr>
+                                          <th scope="row">1</th>
+                                          <td>Mark</td>
+                                          <td>Otto</td>
+                                          <td>@mdo</td>
+                                          <td>@mdo</td>
+                                        </tr>
+                                        <tr>
+                                          <th scope="row">2</th>
+                                          <td>Jacob</td>
+                                          <td>Thornton</td>
+                                          <td>@fat</td>
+                                          <td>@fat</td>
+                                        </tr>
+                                        <tr>
+                                          <th scope="row">3</th>
+                                          <td>Larry</td>
+                                          <td>the Bird</td>
+                                          <td>@twitter</td>
+                                          <td>@twitter</td>
+                                        </tr>
+                                      </tbody>
+                                    </table>
+                                </div>
+                            </div>
+
+                            <div class="card-header"></div>
+
 
                         </form> <!-- End form -->
                    </div> <!-- End card-body -->
